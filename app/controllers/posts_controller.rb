0effec24118412d1b68029ede40ca2a6bf0850ea
rockbook1105 <rockbook1105.xxx@gmail.com>
2019-01-20 14:10:23 +0900

    create projects

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,17 @@
+class PostsController < ApplicationController
+  def index
+    @post = Post.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |n|
+  Post.create(title: "この記事は#{n}回目です。", text: "次回は#{n + 1}の記事です。")
+end
